d39f2307d4b05195c8fba4b9ad30e0864c707163
EvgeniiVasiukov <133101575+EvgeniiVasiukov@users.noreply.github.com>
2024-12-05 19:30:28 +0100

    Update Plant.java

--- Island_test1/src/Models/Plant.java
+++ Island_test1/src/Models/Plant.java
@@ -11,16 +11,16 @@ public class Plant extends IslandObject {
         super(1, 200);
     }
     public void reproduce(Cell currentCell, Island island) {
-        int chance = ThreadLocalRandom.current().nextInt(100);  // Случайное число от 0 до 99
-        if (chance < 10) { // 20% шанс размножения (можно варьировать)
+        int chance = ThreadLocalRandom.current().nextInt(100);  // Random number from 0 to 99
+        if (chance < 10) { // 10% chance of reproducing
             System.out.println("Plant is reproducing.");
 
-            // Создание нового растения
+            // Creating new plant
             Plant newPlant = new Plant();
 
-            // Добавляем новое растение на ту же клетку, где оно размножается
+            // Add a new plant to the same cell where it reproduces
             if (currentCell.canAddObject(newPlant)) {
-                currentCell.addObject(newPlant); // Добавляем новое растение на клетку
+                currentCell.addObject(newPlant); // Adding a new plant to the cell
                 System.out.println("New plant added to cell (" + currentCell.getX() + ", " + currentCell.getY() + ").");
             } else {
                 System.out.println("Cannot add new plant: cell is full.");
