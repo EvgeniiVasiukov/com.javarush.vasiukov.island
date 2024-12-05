24334e35a094694c8d2dd586d59de56188f9ed83
EvgeniiVasiukov <133101575+EvgeniiVasiukov@users.noreply.github.com>
2024-12-05 19:27:12 +0100

    Update Eagle.java

--- Island_test1/src/Models/Predator/Eagle.java
+++ Island_test1/src/Models/Predator/Eagle.java
@@ -3,7 +3,7 @@ package Models.Predator;
 public class Eagle extends Predator {
 
     public Eagle() {
-        super(5.0, 10, 6, 7.0, 1.2, "\uD83E\uDD85"); // вес: 5.0, макс. на клетке: 10, скорость: 6, макс. сытость: 7.0
+        super(5.0, 100, 6, 17.0, 0.2, "\uD83E\uDD85");
     }
 
 }
