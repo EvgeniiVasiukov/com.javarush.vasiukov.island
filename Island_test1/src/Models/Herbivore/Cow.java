5540941a1856bf6d618c4a81806e61c6465c168e
EvgeniiVasiukov <133101575+EvgeniiVasiukov@users.noreply.github.com>
2024-12-05 19:22:26 +0100

    Update Cow.java

--- Island_test1/src/Models/Herbivore/Cow.java
+++ Island_test1/src/Models/Herbivore/Cow.java
@@ -3,6 +3,6 @@ package Models.Herbivore;
 public class Cow extends Herbivore {
 
     public Cow() {
-        super(600.0, 2, 3, 60.0, 7.0, "\uD83D\uDC03"); // вес: 600, макс. на клетке: 2, скорость: 3, макс. сытость: 60.0
+        super(600.0, 20, 3, 60.0, 7.0, "\uD83D\uDC03");
     }
 }
