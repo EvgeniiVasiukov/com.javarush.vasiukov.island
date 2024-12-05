66761f6686e60a309e7a57ed34c8e87baa4bb7f4
EvgeniiVasiukov <133101575+EvgeniiVasiukov@users.noreply.github.com>
2024-12-05 19:27:44 +0100

    Update Predator.java

--- Island_test1/src/Models/Predator/Predator.java
+++ Island_test1/src/Models/Predator/Predator.java
@@ -4,87 +4,59 @@ import Models.Abstraction.Animal;
 import Models.Abstraction.IslandObject;
 import Models.Herbivore.Herbivore;
 import Models.Island.Cell;
-import Models.Island.Island;
 import Models.Utils.EatingChancesSource;
+import Simulation.Simulation;
 
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.Consumer;
 
 public abstract class Predator extends Animal {
-
-    protected Predator(double weight, int maxCountOnLocation, int travelSpeed, double maxSaturation, double hungerRate, String unicodeSymbol) {
+    public Predator(double weight, int maxCountOnLocation, int travelSpeed, double maxSaturation, double hungerRate, String unicodeSymbol) {
         super(weight, maxCountOnLocation, travelSpeed, maxSaturation, hungerRate, unicodeSymbol);
     }
-    private Consumer<Animal> onDeathCallback; // Обработчик события смерти
 
-    public void setOnDeathCallback(Consumer<Animal> onDeathCallback) {
-        this.onDeathCallback = onDeathCallback;
-    }
-
-    // Логика питания хищника
     @Override
-    public void eat(IslandObject food, Cell currentCell) {
-        double initialSaturation = this.getCurrentSaturation();
+    public void eat(IslandObject food, Cell currentCell, Simulation simulation) {
+        double initialSaturation = this.getCurrentSaturation();  // Saturation level before eating
 
+        // Проверяем, что еда является травоядным
         if (food instanceof Herbivore) {
-            int chance = EatingChancesSource.getEatingChance(this.getClass(), food.getClass());
-            int roll = ThreadLocalRandom.current().nextInt(100);
+            int chance = EatingChancesSource.getEatingChance(this.getClass(), food.getClass());  // Get a chance to eat
+            int roll = ThreadLocalRandom.current().nextInt(100);  // Random number from 0 to 99
 
+            // If the chance to eat has passed
             if (roll < chance) {
-                this.increaseSaturation(food.getWeight());
+                this.increaseSaturation(food.getWeight());  // Increasing predator saturation
+                currentCell.removeObject(food);  // Remove the eaten herbivore from the cell
 
-                // Проверяем, является ли съеденное животным, и вызываем обратный вызов
-                if (food instanceof Animal) {
-                    Animal eatenAnimal = (Animal) food;
-                    if (eatenAnimal.getOnDeathCallback() != null) {
-                        eatenAnimal.getOnDeathCallback().accept(eatenAnimal, true); // Отмечаем, что животное было съедено
-                    }
-                    currentCell.removeObject(food);
-                }
-
+                // We log that a predator ate a herbivore
                 System.out.println(this.getUnicodeSymbol() + " (ID: " + this.getId() + ") eats " + food.getClass().getSimpleName() +
                         " and gains " + String.format("%.1f", food.getWeight()) + " saturation (" +
                         formatSaturation(initialSaturation, this.getCurrentSaturation()) + ").");
+
+                // Updating statistics on animals eaten
+                if (food instanceof Animal) {
+                    Animal eatenAnimal = (Animal) food;
+                    eatenAnimal.getOnDeathCallback().accept(eatenAnimal, true);  // Помечаем как съеденное
+                }
+
             } else {
+                // If the meal fails, the predator loses its satiation.
                 this.decreaseSaturation(this.getHungerRate());
                 System.out.println(this.getUnicodeSymbol() + " (ID: " + this.getId() + ") tried to eat " + food.getClass().getSimpleName() +
                         " but failed (" + formatSaturation(initialSaturation, this.getCurrentSaturation()) + ").");
             }
         } else {
+            // If the food is not herbivorous, the predator loses its satiation
             this.decreaseSaturation(this.getHungerRate());
             System.out.println(this.getUnicodeSymbol() + " (ID: " + this.getId() + ") could not find food and lost " +
                     String.format("%.1f", this.getHungerRate()) + " saturation (" +
                     formatSaturation(initialSaturation, this.getCurrentSaturation()) + ").");
         }
 
-        // Проверка на голодную смерть
-        if (this.isDead()) {
-            if (onDeathCallback != null) {
-                onDeathCallback.accept(this);
-            }
-            currentCell.removeObject(this);
-            //System.out.println(this.getUnicodeSymbol() + " (ID: " + this.getId() + ") has died of hunger.");
-        }
+        // Check if the animal has died of hunger and update the statistics via Simulation
+        checkIfDead(currentCell, simulation);  // Pass the simulation object to update statistics
     }
 
 
 
-
-
-
-
-
-
-
-    // Логика размножения для хищника (использует общую логику из класса Animal)
-    @Override
-    public void reproduce(Cell currentCell, Island island) {
-        super.reproduce(currentCell, island); // Используем общую логику из Animal
-    }
-
-    // Логика движения хищника (использует логику движения из Animal)
-    @Override
-    public void move(Cell currentCell, Cell targetCell) {
-        super.move(currentCell, targetCell); // Используем общую логику движения из Animal
-    }
 }
