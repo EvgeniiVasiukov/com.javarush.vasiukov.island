1dac402bea5b658596184bf6455893453957c1cc
EvgeniiVasiukov <133101575+EvgeniiVasiukov@users.noreply.github.com>
2024-12-05 19:25:49 +0100

    Update Rabbit.java

--- Island_test1/src/Models/Herbivore/Rabbit.java
+++ Island_test1/src/Models/Herbivore/Rabbit.java
@@ -3,6 +3,6 @@ package Models.Herbivore;
 public class Rabbit extends Herbivore {
 
     public Rabbit() {
-        super(2.0, 500, 4, 3.0, 0.6, "\uD83D\uDC07"); // вес: 2.0, макс. на клетке: 500, скорость: 4, макс. сытость: 3.0
+        super(2.0, 500, 4, 3.0, 0.6, "\uD83D\uDC07");
     }
 }
