3264b444bc3501fb3d64ee3adf60d25165eb03e8
EvgeniiVasiukov <133101575+EvgeniiVasiukov@users.noreply.github.com>
2024-12-05 19:25:02 +0100

    Update Herbivore.java

--- Island_test1/src/Models/Herbivore/Herbivore.java
+++ Island_test1/src/Models/Herbivore/Herbivore.java
@@ -3,54 +3,38 @@ package Models.Herbivore;
 import Models.Abstraction.Animal;
 import Models.Abstraction.IslandObject;
 import Models.Island.Cell;
-import Models.Island.Island;
 import Models.Plant;
-
-import java.util.function.Consumer;
+import Simulation.Simulation;
 
 public abstract class Herbivore extends Animal {
-
-    protected Herbivore(double weight, int maxCountOnLocation, int travelSpeed, double maxSaturation, double hungerRate, String unicodeSymbol) {
+    public Herbivore(double weight, int maxCountOnLocation, int travelSpeed, double maxSaturation, double hungerRate, String unicodeSymbol) {
         super(weight, maxCountOnLocation, travelSpeed, maxSaturation, hungerRate, unicodeSymbol);
     }
-    private Consumer<Animal> onDeathCallback; // Обработчик события смерти
 
-    public void setOnDeathCallback(Consumer<Animal> onDeathCallback) {
-        this.onDeathCallback = onDeathCallback;
-    }
-
-    // Логика питания для травоядного
     @Override
-    public void eat(IslandObject food, Cell currentCell) {
-        double initialSaturation = this.getCurrentSaturation(); // Уровень насыщения до еды
+    public void eat(IslandObject food, Cell currentCell, Simulation simulation) {
+        double initialSaturation = this.getCurrentSaturation(); // Saturation level before eating
 
         if (food instanceof Plant) {
-            this.increaseSaturation(food.getWeight());
-            currentCell.removeObject(food); // Удаляем съеденное растение
+            increaseSaturation(food.getWeight()); // Increasing saturation
+            currentCell.removeObject(food); // Removing plant from the cell
+
+            // Logging that animal ate plant
             System.out.println(this.getUnicodeSymbol() + " (ID: " + this.getId() + ") eats a plant and gains " +
                     String.format("%.1f", food.getWeight()) + " saturation (" +
                     formatSaturation(initialSaturation, this.getCurrentSaturation()) + ").");
         } else {
-            this.decreaseSaturation(this.getHungerRate());
+            decreaseSaturation(getHungerRate()); // If food is not found saturation is lost
+
+            // Logging that animal could not find food
             System.out.println(this.getUnicodeSymbol() + " (ID: " + this.getId() + ") could not find food and lost " +
-                    String.format("%.1f", this.getHungerRate()) + " saturation (" +
+                    String.format("%.1f", getHungerRate()) + " saturation (" +
                     formatSaturation(initialSaturation, this.getCurrentSaturation()) + ").");
         }
 
-        // Проверяем, умерло ли травоядное
-        if (this.isDead()) {
-            if (onDeathCallback != null) {
-                onDeathCallback.accept(this); // Сообщаем о смерти
-            }
-            currentCell.removeObject(this); // Удаляем животное из клетки
-            //System.out.println(this.getUnicodeSymbol() + " (ID: " + this.getId() + ") has died of hunger.");
-        }
+        // We check if the animal has died of hunger and update the statistics via Simulation
+        checkIfDead(currentCell, simulation);
     }
 
 
-    // Логика размножения для травоядного (использует общую логику из класса Animal)
-    @Override
-    public void reproduce(Cell currentCell, Island island) {
-        super.reproduce(currentCell, island); // Используем общую логику из Animal
-    }
 }
