ee3ae77d91accc178d60179c13cace6788e69b6d
EvgeniiVasiukov <133101575+EvgeniiVasiukov@users.noreply.github.com>
2024-12-05 19:23:53 +0100

    Update Deer.java

--- Island_test1/src/Models/Herbivore/Deer.java
+++ Island_test1/src/Models/Herbivore/Deer.java
@@ -3,6 +3,6 @@ package Models.Herbivore;
 public class Deer extends Herbivore {
 
     public Deer() {
-        super(150.0, 2, 2, 35.0, 7.0, "\uD83E\uDD8C"); // Вес: 150, макс. на клетке: 2, скорость: 2, макс. сытость: 35.0
+        super(150.0, 20, 2, 35.0, 7.0, "\uD83E\uDD8C");
     }
 }
