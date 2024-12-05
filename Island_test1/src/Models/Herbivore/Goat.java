c6b22f0bbc1824af120bd77105b94cffd6a1cef3
EvgeniiVasiukov <133101575+EvgeniiVasiukov@users.noreply.github.com>
2024-12-05 19:24:28 +0100

    Update Goat.java

--- Island_test1/src/Models/Herbivore/Goat.java
+++ Island_test1/src/Models/Herbivore/Goat.java
@@ -3,6 +3,6 @@ package Models.Herbivore;
 public class Goat extends Herbivore {
 
     public Goat() {
-        super(70.0, 20, 3, 5.0, 1.5,"\uD83D\uDC10"); // вес: 70.0, макс. на клетке: 20, скорость: 3, макс. сытость: 5.0
+        super(70.0, 20, 3, 5.0, 1.5,"\uD83D\uDC10");
     }
 }
